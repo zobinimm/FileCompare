29b14c6818e58e20498db5d4eec2d9154aaf24db
zobinimm <z0bin@163.com>
2024-05-28 14:44:56 +0800

    FileCompare

---
+++ FileCompare/MainForm.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace FileCompare
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPath2 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPath1
+            // 
+            this.txtPath1.Location = new System.Drawing.Point(58, 24);
+            this.txtPath1.Name = "txtPath1";
+            this.txtPath1.Size = new System.Drawing.Size(641, 19);
+            this.txtPath1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Path1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Path2:";
+            // 
+            // txtPath2
+            // 
+            this.txtPath2.Location = new System.Drawing.Point(58, 49);
+            this.txtPath2.Name = "txtPath2";
+            this.txtPath2.Size = new System.Drawing.Size(641, 19);
+            this.txtPath2.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 74);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(371, 364);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyDown);
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2});
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(389, 74);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(371, 364);
+            this.listView2.TabIndex = 5;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView2_KeyDown);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(705, 24);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(55, 44);
+            this.btnRun.TabIndex = 6;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Files";
+            this.columnHeader1.Width = 370;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Files";
+            this.columnHeader2.Width = 370;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PowderBlue;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(201, 120);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(353, 177);
+            this.panel1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(64, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 64);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Waiting";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPath2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FileCompare";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPath2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button btnRun;
+        private System.ComponentModel.BackgroundWorker bgWorker;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FileCompare/MainForm.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileCompare
+{
+    public partial class MainForm : Form
+    {
+        private delegate void DelegateSetFormEnable(bool isEnable);
+        private delegate void DelegateSetList();
+
+        private ConcurrentDictionary<string, (long size, string hash)> fileHashes1;
+        private ConcurrentDictionary<string, (long size, string hash)> fileHashes2;
+
+        private List<string> fileName1;
+        private List<string> fileName2;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            bgWorker.WorkerReportsProgress = true;
+            bgWorker.WorkerSupportsCancellation = true;
+            bgWorker.DoWork += BgWorker_DoWork;
+            bgWorker.RunWorkerCompleted += BgWorker_RunWorkerCompleted;
+            panel1.Visible = false;
+        }
+
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Process was cancelled", "Process Cancelled");
+            }
+            else
+            {
+                SetListInTask();
+            }
+            SetFormEnableInTask(true);
+        }
+
+        private void BgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            SetFormEnableInTask(false);
+            string folderPath1 = @txtPath1.Text;
+            string folderPath2 = @txtPath2.Text;
+
+            fileHashes1 = GetFileHashes(folderPath1);
+            fileHashes2 = GetFileHashes(folderPath2);
+
+            foreach (var file1 in fileHashes1)
+            {
+                foreach (var file2 in fileHashes2)
+                {
+                    if (file1.Value == file2.Value)
+                    {
+                        if (!fileName1.Contains(file1.Key)) fileName1.Add(file1.Key);
+                        if (!fileName2.Contains(file2.Key)) fileName2.Add(file2.Key);
+                    }
+                }
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            fileName1 = new List<string>();
+            fileName2 = new List<string>();
+            if (bgWorker.IsBusy != true)
+            {
+                bgWorker.RunWorkerAsync();
+            }
+        }
+
+        private void SetFormEnableInTask(bool isEnable)
+        {
+            try
+            {
+                DelegateSetFormEnable delegateSetFormEnable = new DelegateSetFormEnable(SetFormEnable);
+                this.Invoke(delegateSetFormEnable, isEnable);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void SetFormEnable(bool isEnable)
+        {
+            foreach (Control control in Controls)
+            {
+                if (control != panel1)
+                {
+                    control.Enabled = isEnable;
+                }
+            }
+            panel1.Visible = !isEnable;
+        }
+
+        private void SetListInTask()
+        {
+            try
+            {
+                DelegateSetList delegateSetList = new DelegateSetList(SetList);
+                this.Invoke(delegateSetList);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void SetList()
+        {
+            listView1.Items.Clear();
+            listView1.Items.AddRange(fileName1.ConvertAll(x => new ListViewItem(x)).ToArray());
+            listView2.Items.Clear();
+            listView2.Items.AddRange(fileName2.ConvertAll(x => new ListViewItem(x)).ToArray());
+        }
+
+
+        private ConcurrentDictionary<string, (long size, string hash)> GetFileHashes(string folderPath)
+        {
+            var fileHashes = new ConcurrentDictionary<string, (long size, string hash)>();
+            var files = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
+
+            Parallel.ForEach(files, filePath =>
+            {
+                var fileInfo = new FileInfo(filePath);
+                string hash = ComputeMD5(filePath);
+                fileHashes[filePath] = (fileInfo.Length, hash);
+            });
+
+            return fileHashes;
+        }
+
+        private string ComputeMD5(string filePath)
+        {
+            var md5 = HashLib.HashFactory.Crypto.CreateMD5();
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                var buffer = new byte[8192];
+                int bytesRead;
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    md5.TransformBytes(buffer, 0, bytesRead);
+                }
+            }
+            var hashBytes = md5.TransformFinal().GetBytes();
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+        }
+
+        private void listView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (ListViewItem item in listView1.SelectedItems)
+                {
+                    sb.AppendLine(item.Text);
+                }
+                Clipboard.SetDataObject(sb.ToString());
+            }
+            else if (e.Control && e.KeyCode == Keys.A)
+            {
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    item.Selected = true;
+                }
+            }
+        }
+
+        private void listView2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (ListViewItem item in listView2.SelectedItems)
+                {
+                    sb.AppendLine(item.Text);
+                }
+                Clipboard.SetDataObject(sb.ToString());
+            }
+            else if (e.Control && e.KeyCode == Keys.A)
+            {
+                foreach (ListViewItem item in listView2.Items)
+                {
+                    item.Selected = true;
+                }
+            }
+        }
+    }
+}
